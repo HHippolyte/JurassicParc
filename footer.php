d6bc1c8658ca723e10763c6ec623607fea2b79bf
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-03-16 22:32:23 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer>
+	<section>
+		<div class="colonne centrer col12">
+			<h2>Contactez-nous</h2>
+		</div>
+			<!-- Ici je mettrais une partie PHP pour l'envoi de messages ;)-->
+	</section>
+	<section>
+		<div class="colonne centrer col12">
+				<h2>Réseaux sociaux</h2>
+		</div>
+	</section>
+	<section class="centrer">
+		<div class="colonne_alpha col3">
+			<img src="Style/images/twitter.png" width="70" height="70" alt="Twitter">
+		</div>
+		<div class="colonne col3">
+			<img src="Style/images/facebook.png" width="65" height="65" alt="Facebook">
+		</div>
+		<div class="colonne col3">
+			<img src="Style/images/instagram.png" width="70" height="70" alt="Intsagram">
+		</div>
+		<div class="colonne col3">
+			<img src="Style/images/email.png" width="70" height="70" alt="Email">
+		</div>
+	</section>
+</footer>
---
+++ header.php
@@ -0,0 +1,27 @@
+	<header>
+		<!-- Logo -->
+		<div>
+			<a class="colonne_alpha centrer col12 logo" href="index.php"><img src="Style/images/logo.png" alt="Logo du Zoo" /></a>
+							
+				<!--navigation:Menu déroulant placé à droite de manière permanente-->
+							
+				<ul class="menu">
+					<li> MENU
+						<ul class=sousmenu1>
+							<li>
+								<a href="presentation.php">Présentation du zoo<a/>
+							</li>
+							<li>
+								<a href="infos.php">Infos pratiques</a>
+							</li>
+							<li>
+								<a href="reservations.php">Réservations</a>
+							</li>
+							<li>
+								<a href="intranet.php">Intranet </a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+		</div>
+	</header>	
