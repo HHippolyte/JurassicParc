48d7e6930aae22cbf68c5ffe5d522cc0cb4cb5c1
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-03-23 09:51:34 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,58 +1,63 @@
+
 <footer>
 	<section>
 		<div class="colonne centrer col12">
 			<h2>Contactez-nous</h2>
-
-<table>
-<form action="envoi.php" method="post" >
-<tr> 
-<td colspan="3"><strong>Envoyer un message</strong></td>
-</tr>
-<tr> 
-<td><div align="left">Votre nom :</div></td>
-<td><input type="text" name="nom" size="45" maxlength="100"></td>
-</tr>
-<tr> 
-<td><div align="left">Votre mail :</div></td>
-<td><input type="text" name="mail" size="45" maxlength="100"></td>
-</tr>
-<tr> 
-<td><div align="left">Sujet : </div></td>
-<td><input type="text" name="objet" size="45" maxlength="120"></td>
-</tr>
-<tr> 
-<td><div align="left">Message : </div></td>
-<td><textarea name="message" maxlength="500"></textarea></td>
-</tr>
-<td><center>
-<input type="reset" name="Submit" value="Annuler">
-</center></td>
-<td><center>
-<input type="submit" name="Submit" value="Envoyer">
-</center></td>
-</tr>
-</form>
-</table>
-
 		</div>
-	</section>
-	<section>
-		<div class="colonne centrer col12">
+			<!-- Ici je mettrais une partie PHP pour l'envoi de messages ;)-->
+	<div class="colonne_alpha col6"
+		<table>
+			<form action="contact.php" method="post" >
+				<tr> 
+					<td colspan="3"><strong>Envoyer un message</strong></td>
+				</tr>
+				<tr> 
+					<td><div align="left">Votre nom :</div></td>
+					<td><input type="text" name="nom" size="45" maxlength="100"></td>
+				</tr>
+				<tr> 
+					<td><div align="left">Votre mail :</div></td>
+					<td><input type="text" name="mail" size="45" maxlength="100"></td>
+				</tr>
+				<tr> 
+					<td><div align="left">Sujet : </div></td>
+					<td><input type="text" name="objet" size="45" maxlength="120"></td>
+				</tr>
+				<tr> 
+					<td><div align="left">Message : </div></td>
+					<td><textarea name="message" maxlength="500"></textarea></td>
+				</tr>
+			<section>
+				<tr>
+				
+						<td>
+
+							<input type="reset" name="Submit" value="Annuler">
+						</td>
+						<td>
+							<input type="submit" name="Submit" value="Envoyer">
+						</td>
+				</tr>
+			</section>
+			</form>
+		</table>
+	</div>
+
+		<div class="colonne col6">
 				<h2>Réseaux sociaux</h2>
-		</div>
-	</section>
-	<section class="centrer">
-		<div class="colonne_alpha col3">
+		
+		<div class="icone colonne col2">
 			<img src="Style/images/twitter.png" width="70" height="70" alt="Twitter">
 		</div>
-		<div class="colonne col3">
+		<div class="icone colonne col2">
 			<img src="Style/images/facebook.png" width="65" height="65" alt="Facebook">
 		</div>
-		<div class="colonne col3">
+		<div class="icone colonne col2">
 			<img src="Style/images/instagram.png" width="70" height="70" alt="Intsagram">
 		</div>
-		<div class="colonne col3">
+		<div class="icone colonne col2">
 			<img src="Style/images/email.png" width="70" height="70" alt="Email">
 		</div>
 	</section>
+
 </footer>
