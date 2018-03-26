5bffd97b67613dc1e55d065dfbf2737ab56adc3b
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-03-26 17:00:25 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -13,111 +13,59 @@
 				<!-- Header -->
 					<?php include("header.php"); ?> 
 							<!-- Logo -->
-					<div> 
+					<div class="formulaire"> 
 						<form method="post">
-						<p><center><legend><h3>Espace employé</h3> </legend></center></p>
+						<p><center><legend><h1>Espace employé</h1> </legend></center></p>
 
 								<p>
-									<label for="code_employe"> Votre numéro d'employé : </label>
+									<label for="code_employe"> <h3>Votre numéro d'employé :</h3> </label>
 									<input type="text" name="CODE_EMPLOYE">
-									<label for="pass_employe" >Votre mot de passe : </label>
+									<label for="pass_employe" ><h3>Votre mot de passe :</h3> </label>
 									<input type="password" name="PASS_EMPLOYE">
 									<input type="submit" value='envoyer'>
 								</p>
 						</form>
-				</div>
-						<!-- Deux espaces membres différents -->
-				<div> 
-						<form method="get">
-						<p><center><legend><h3>Espace Vétérinaire</h3> </legend></center></p>
+					</div>
 
-								<p>
-									<label for="code_veto"> Votre code ordinal : </label>
-									<input type="text" name="code_veto">
-									<label for="pass_veto" >Votre mot de passe : </label>
-									<input type="password" name="pass_veto">
-									<input type="submit" value='envoyer'>
-								</p>
-						</form>
-				</div>
+				<?php
+					//Connexion au serveur MySQL et sélection de la base
+					$connexion=mysqli_connect("localhost","root","","zoopedia")
+					or die(mysql_error());
 
-<?php
+					//Vérification que le formulaire n'est pas vide
+					if(!empty($_POST))
+					{
+					//Renommer les données reçues du formulaire
+					$codemp=$_POST['CODE_EMPLOYE'];
+					$passemp=$_POST['PASS_EMPLOYE'];
+							//Fabrication de la requête SQL
+							$requete_connexion='SELECT * FROM employe';
 
+							//Envoi de la requête SELECT
+							$result_connexion=mysqli_query($connexion,$requete_connexion) or die(mysql_error());
 
-//Connexion au serveur MySQL et sélection de la base
-$connexion=mysqli_connect("localhost","root","","zoopedia")
-or die(mysql_error());
+							//Récupération des données de la requête
+							$data_connexion=mysqli_fetch_array($result_connexion,MYSQLI_ASSOC);
 
-//Vérification que le formulaire n'est pas vide
-if(!empty($_POST))
-{
-//Renommer les données reçues du formulaire
-$codemp=$_POST['CODE_EMPLOYE'];
-$passemp=$_POST['PASS_EMPLOYE'];
-		//Fabrication de la requête SQL
-		$requete_connexion='SELECT * FROM employe';
+								if ($codemp==$data_connexion['CODE_EMPLOYE'] and $passemp==$data_connexion['PASS_EMPLOYE'] )
+								{
+									session_start();
+								    $_SESSION['nom'] = $data_connexion['NOM_EMPLOYE'];
+								    $_SESSION['prenom'] = $data_connexion['PRENOM_EMPLOYE'];
+								    $_SESSION['numemploye'] = $data_connexion['CODE_EMPLOYE'];
+								    header('Location: employe.php');
+								    
 
-		//Envoi de la requête SELECT
-		$result_connexion=mysqli_query($connexion,$requete_connexion) or die(mysql_error());
+								}
+								else
+								{
+								    echo 'Mauvais identifiant ou mot de passe !';
+							    }
+					}
 
-		//Récupération des données de la requête
-		$data_connexion=mysqli_fetch_array($result_connexion,MYSQLI_ASSOC);
-
-			if ($codemp==$data_connexion['CODE_EMPLOYE'] and $passemp==$data_connexion['PASS_EMPLOYE'] )
-			{
-				session_start();
-			    $_SESSION['nom'] = $data_connexion['NOM_EMPLOYE'];
-			    $_SESSION['prenom'] = $data_connexion['PRENOM_EMPLOYE'];
-			    $_SESSION['numemploye'] = $data_connexion['CODE_EMPLOYE'];
-			    header('Location: employe.php');
-			    
-
-			}
-			else
-			{
-			    echo 'Mauvais identifiant ou mot de passe !';
-		    }
-}
-
-?>
-
-<!-- Pour le deuxième formulaire -->
-<?php
-
-//Connexion au serveur MySQL et sélection de la base
-$connexion=mysqli_connect("localhost","root","","zoopedia")
-or die(mysql_error());
-
-//Vérification que le formulaire n'est pas vide
-if(!empty($_GET))
-{
-//Renommer les données reçues du formulaire
-$codevet=$_GET['code_veto'];
-$passvet=$_GET['pass_veto'];
-		//Fabrication de la requête SQL
-		$requete_connexion='SELECT * FROM veterinaire';
-
-		//Envoi de la requête SELECT
-		$result_connexion=mysqli_query($connexion,$requete_connexion) or die(mysql_error());
-
-		//Récupération des données de la requête
-		$data_connexion=mysqli_fetch_array($result_connexion,MYSQLI_ASSOC);
-			if ($codevet==$data_connexion['CODE_ORDINAL_VETERINAIRE'] and $passvet==$data_connexion['PASS_VETO'] )
-			{
-				session_start();
-			    $_SESSION['nom'] = $data_connexion['NOM_VETERINAIRE'];
-			    $_SESSION['prenom'] = $data_connexion['PRENOM_VETERINAIRE'];
-			    $_SESSION['numveto'] = $data_connexion['CODE_ORDINAL_VETERINAIRE'];
-			    header('Location: veterinaire.php');
-			    
-
-			}
-			else
-			{
-			    echo 'Mauvais identifiant ou mot de passe !';
-		    }
-}
-?>
+					?>						
 			</div>
+			<!-- Footer-->
+			<?php include("footer.php");?>
 	</body>
 </html>
