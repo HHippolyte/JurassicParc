5a95b11bb10f35e963c44c20f684200e26ad6e8a
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-03-28 16:40:04 +0200

    Update infos.php

--- infos.php
+++ infos.php
@@ -16,43 +16,58 @@
 
 				<!-- Généralités -->
 					<main>
+						
 						<div class="colonne_alpha col12 centrer presentation">
-							<h1> Infos pratiques </h1>
-							<span class="infospratiques"><img src="Style/images/infos.png" alt=" Infos pratiques" /></span>
-							<h2>Horaires et tarifs </h2>
-							<p> Notre parc est accessible aux personnes à mobilité réduite et aires de pique-nique sont à disposition. <br/>
-							Le snack est fermé pour l'hiver, ouverture le 1er mars 2018 ! <br/>
-							Possibilité de sortir pour accéder aux voitures (pique-nique...) en gardant précieusement le ticket d'entrée. 
-							</p>
-							<h3>Horaires</h3>
-							<p>OUVERT TOUS LES JOURS, DIMANCHES ET JOURS FÉRIÉS INCLUS !</p>
+							<div class="cadre">	
+									<div class="titre">
+									 Infos pratiques 
+									</div>
+									<div class="infospratiques">
+									<img src="Style/images/infos.png" alt=" Infos pratiques" />
+									</div>
+									<h2>Horaires et tarifs </h2>
+									<p> Notre parc est accessible aux personnes à mobilité réduite et aires de pique-nique sont à disposition. <br/>
+									Le snack est fermé pour l'hiver, ouverture le 1er mars 2018 ! <br/>
+									Possibilité de sortir pour accéder aux voitures (pique-nique...) en gardant précieusement le ticket d'entrée. 
+									</p>
+									<h3>Horaires</h3>
+									<p>OUVERT TOUS LES JOURS, DIMANCHES ET JOURS FÉRIÉS INCLUS !</p>
+							</div>
 						</div>
-						<div class="colonne_alpha col6 ">
-							<h3>HORAIRES D'HIVER :</h3>
-							<p>Du 29 octobre 2017 au 24 mars 2018</p> 
-							<h4>OUVERTURE DES CAISSES :	</h4>
-							<p>de 10h00 à 16h00</p> 
-							<p>Fermeture du parc à 17h00.</p>
-						</div>
-						<div class="colonne_alpha col6 ">
-							<h3>HORAIRES D'ÉTÉ :</h3> 
-							<p>du 25 mars 2018 au 27 octobre 2018 </p>
-							<h4>OUVERTURE DES CAISSES :</h4>				
-							<p>de 9h00 à 18h00 </p>
-							<p>Fermeture du parc à 19h00.</p>
-						</div>
-						<div class="colonne col12">			
-							<h3>Fermeture du parc sans préavis possible selon les conditions climatiques :</h3>
-							<p>neige/gel... Fermé le 25 Décembre.</p>
-							<p>Note : Les animaux sont rentrés petit à petit au cours de la dernière heure avant la fermeture.</p>
-							<h3> Nos tarifs </h3> 
-							<p> Moins de 3 ans : gratuit <br/>
-							Jusqu'à 10 ans : 11,00€ <br/>
-							Plus de 10 ans : 15,00€ <br/></p>
+						
+							<div class="colonne_alpha col6 ">
+								<div class="cadre">
+								<h3>HORAIRES D'HIVER :</h3>
+								<p>Du 29 octobre 2017 au 24 mars 2018</p> 
+								<h4>OUVERTURE DES CAISSES :	</h4>
+								<p>de 10h00 à 16h00</p> 
+								<p>Fermeture du parc à 17h00.</p>
+								</div>
+							</div>
+							<div class="colonne_alpha col6 ">
+								<div class="cadre">
+								<h3>HORAIRES D'ÉTÉ :</h3> 
+								<p>du 25 mars 2018 au 27 octobre 2018 </p>
+								<h4>OUVERTURE DES CAISSES :</h4>				
+								<p>de 9h00 à 18h00 </p>
+								<p>Fermeture du parc à 19h00.</p>
+								</div>
+							</div>
+							<div class="colonne col12">
+								<div class="cadre">			
+								<h3>Fermeture du parc sans préavis possible selon les conditions climatiques :</h3>
+								<p>neige/gel... Fermé le 25 Décembre.</p>
+								<p>Note : Les animaux sont rentrés petit à petit au cours de la dernière heure avant la fermeture.</p>
+								<h3> Nos tarifs </h3> 
+								<p> Moins de 3 ans : gratuit <br/>
+								Jusqu'à 10 ans : 11,00€ <br/>
+								Plus de 10 ans : 15,00€ <br/></p>
 
-							<h3>Modes de règlement :</h3>
-						<p>Espèces, Chèques, Cartes Bancaires, Chèques Vacances : acceptés pour l'entrée du parc uniquement, monnaie non rendue, non valables sans le talon (partie haute)</p>
-						</div>
+								<h3>Modes de règlement :</h3>
+							<p>Espèces, Chèques, Cartes Bancaires, Chèques Vacances : acceptés pour l'entrée du parc uniquement, monnaie non rendue, non valables sans le talon (partie haute)</p>
+								</div>
+							</div>
+						
 					</main>
 
 				<!-- Footer -->
