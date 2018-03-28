5672227517cae85f89a693586373a14c3d6ee1d6
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-03-28 12:39:09 +0200

    Update employe.php

--- employe.php
+++ employe.php
@@ -10,76 +10,62 @@ session_start();
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" href="structure/structure-main.css"/>
-		<link rel="stylesheet" href="Style/Styleemploye.css"/>
+		<link rel="stylesheet" href="Style/Style.css"/>
 	</head>
 	<body>
 			<div id="conteneur"> <!-- ex wrapper correspond à l'ensemble du corps --!>
 				<!-- Header -->
 					<?php include("header.php"); ?> 
-<?php 
-echo 'Vous êtes connecté '.$_SESSION['nom'].' '.$_SESSION['prenom'].' !';
-?>
-       <!-- Réalisation d'un menu employé pour la gestion du zoo -->
-			<section> 
-								<div class="colonne_alpha col4" id="profil">
-									<a href="profil.php">
 
-										<center><h2>Mon profil</h2><center>
-										<div class="content">
-											<p>Accédez à votre profil</p><br><br>
-										</div>
-									</a>
-								</div>
-								<div class="colonne col4" id="distribution">
-									<a href="distribution.php">
-										<center><h2> Distribution de la nourriture</h2><center>
-										<div class="content">
-											<p> Carnet de distribution de la nourriture</p><br><br>
-										</div>
-									</a>
-										
-								</div>
-								<div class="colonne col4" id="animaux"> 
-									<a href="placement.php">
-										<center><h2>Placement animal</h2><center>
-										<div class="content">
-											<p> Gestion de l'espace </p><br><br>
-										</div>
-									</a>
-								</div>
-							</section>
-			<section> 
-				<center><div class="colonne col4" id="ajout">
-									<a href="creation.php">
-										<center><h2>Création fiche animal</h2><center>
-										<div class="content">
-											<p> Enregistrer un nouvel arrivant </p><br><br>
-										</div>
-									</a>
-								</div></center>
-						</section>
-					
+					<!-- Page générale -->
+					<main>
+						<div class="contenu">
+						<?php 
+						echo 'Vous êtes connecté '.$_SESSION['nom'].' '.$_SESSION['prenom'].' !';
+						?>
+						</div>
+	       <!-- Réalisation d'un menu employé pour la gestion du zoo -->
+				
+									
+								<section>
+									<div class="profil">
+										<form method="post" action="profil.php">
+											<input type="submit" value=" Accédez à votre profil" />
+										</form>
+									</div>
+								</section>	
+								<section>
 
+									<div class="colonne_alpha col4" id="distribution">
+										<a href="distribution.php">
+											<center><h2> Distribution </h2><center>
+											<div class="contenu">
+												<p> Carnet de distribution de la nourriture</p><br><br>
+											</div>
+										</a>
+											
+									</div>
+									<div class="colonne col4" id="animaux"> 
+										<a href="placement.php">
+											<center><h2>Placement animal</h2><center>
+											<div class="contenu">
+												<p> Gestion de l'espace </p><br><br>
+											</div>
+										</a>
+									</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
-</body>
+									<div class="colonne col4" id="ajout">
+										<a href="creation.php">
+											<center><h2>Création fiche animal</h2>
+											<div class="contenu">
+												<p> Enregistrer un nouvel arrivant </p></center><br><br>
+											</div>
+										</a>
+									</div>
+								</section>
+					</main>
+					<!-- Footer -->
+				<?php include("footer.php");?>				
+			</div>
+	</body>
 </html>
