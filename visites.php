5c3e0423b3a5515d2bbb219788980ce4a1452dcf
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-03-28 16:34:16 +0200

    Add files via upload

---
+++ visites.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title> Visites en groupe - ZooPedia</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="structure/structure-main.css"/>
+		<link rel="stylesheet" href="Style/Style.css"/>
+		
+	</head>
+	<body>
+			<div id="conteneur">
+
+				<!-- Header -->
+				<?php include("header.php");?>
+
+				<!-- Généralités -->
+				<main>
+					<div id="colonne_alpha centrer col12 presentation">
+						<div class="cadre">
+							<div class="titre">
+								Visites de groupes 
+							</div>
+							<div class="presentation">
+								<h1> Présentation </h1>
+								<p>Découvrez les différentes visites guidées pour les groupes (associations, clubs, sorties familiales...) ! 
+								Sur réservation uniquement, hors périodes de vacances scolaires.</p>
+								<img src="Style/images/visites.png" alt="Visites" />
+								<p> Durée : 3h <br/>Tarif : 70 € par groupe (en plus du prix d'entrée). <br/>
+								Entre 15 et 25 personnes maximum. <br/>
+								Réservations par mail : pedagogie.parczoologique@gmail.com </p>
+
+								<p> Profitez d'une visite guidée avec notre animateur pédagogique qui vous racontera les anecdotes du parc et vous permettra de mieux connaître nos animaux emblématiques. </p>
+
+
+								<h1> Fonctionnement </h1>
+								<p> Le parc accepte la venue de groupes, pour plus d'informations ou pour réserver, veuillez nous contacter en fin de page dans la rubrique "Nous contacter". </p>
+							</div>
+						</div>
+					</div>
+				</main>
+
+				<!-- Footer -->
+				<?php include("footer.php");?>
+
+			</div>
+
+	</body>
+</html>
