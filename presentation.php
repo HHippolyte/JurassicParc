6492710c7f839b6e807579d0e8b35040bd506c49
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-03-28 16:41:08 +0200

    Update presentation.php

--- presentation.php
+++ presentation.php
@@ -11,29 +11,37 @@
 		<link rel="stylesheet" href="Style/Style.css"/>
 	</head>
 	<body>
-			<div id="conteneur"> <!-- ex wrapper correspond à l'esnsemble du corps --!>
+			<div id="conteneur"> 
 				<!-- Header -->
 						<?php include("header.php"); ?>
 
 				<!-- Généralités -->
 				<main>
-					<div id="main" class="colonne_alpha centrer col12 presentation">
-					
-						<div class="presentation">
-							<h1> Présentation de ZooPedia </h1>
-							<a href= "Style/images/presentation.png"> <img src="Style/images/presentations-min.png" alt=" Carte du zoo" title ="Cliquez pour voir le plan du zoo"/></a>
-						</div>
-						<div class="contenu">
-							<p> Découvrez le plan de visite de l’Espace Zoologique. </p>
-							<P>Un sens de visite est indiqué afin que vous ne ratiez aucune espèce ! </p>
-						</div>
-						<div class="presentation">
-							<h2> Historique du zoo </h2>
-							<p> L'histoire de ce zoo est assez récente, débutant au début des années 2000. Grandissant rapidement au fil des années, il ouvre en 2018 son premier site web permettant à notre petit monde d'interagir plus facilement avec vous, humbles invités et pour d'autres fidèles habitués. </p>
-							<h2> Présentation </h2>
-							<p> Notre zoo est situé dans un parc protégé proche de Lyon et Genève entre Jura et Alpes. De cette manière vous aurez l'occasion de vous ressourcez dans un cadre naturel tout en apprenant de nouvelles choses.</p>
-						</div>
 
+
+					
+						<div class="colonne_alpha centrer col12 presentation">
+						<div class="cadre">
+							<div class="titre">
+								Présentation de ZooPedia 
+							</div>	
+								<a href= "Style/images/presentation.png"> <img src="Style/images/presentations-min.png" alt=" Carte du zoo" title ="Cliquez pour voir le plan du zoo"/></a>
+							
+							<div class="contenu">
+								<p> Découvrez le plan de visite de l’Espace Zoologique. </p>
+								<P>Un sens de visite est indiqué afin que vous ne ratiez aucune espèce ! </p>
+							</div>
+							
+							<div class="presentation alignegauche ">
+								<h2> Historique du zoo </h2>
+								<p> L'histoire de ce zoo est assez récente, débutant au début des années 2000. Grandissant rapidement au fil des années, il ouvre en 2018 son premier site web permettant à notre petit monde d'interagir plus facilement avec vous, humbles invités et pour d'autres fidèles habitués. </p>
+								<br/>
+								<br/>
+								<h2> Présentation </h2>
+								<p> Notre zoo est situé dans un parc protégé proche de Lyon et Genève entre Jura et Alpes. De cette manière vous aurez l'occasion de vous ressourcez dans un cadre naturel tout en apprenant de nouvelles choses.</p>
+							</div>
+
+						</div>
 					</div>
 				</main>
 
