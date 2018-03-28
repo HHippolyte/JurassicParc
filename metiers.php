1a66f983080af06088c00921ced9f0cbe013780c
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-03-28 16:36:24 +0200

    Add files via upload

---
+++ metiers.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Nos métiers - ZooPedia</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="structure/structure-main.css" />
+		<link rel="stylesheet" href="Style/Style.css"/>
+		
+	</head>
+	<body>
+			<div id="conteneur">
+
+				<!-- Header -->
+				<?php include("header.php");?>
+
+				<!-- Généralités -->
+					<main>
+						<div id="colonne_alpha centrer col12 presentation">
+							<div class="cadre">
+								<div class="titre">
+									Les métiers existants dans notre zoo
+								</div>	
+									<img src="Style/images/metiers.png" alt="Nos métiers" />
+								<div class="presentation alignegauche">
+									<h2> Les soigneurs animaliers </h2>
+									<p>Ils sont responsables des animaux dont ils ont la charge, ils assurent un suivi de chaque individu sur le long terme en tenant à jour l'inventaire des naissances, reproductions, décès... <br/>
+
+									Leur travail consiste à préparer et à distribuer les rations alimentaires, à nettoyer les enclos ainsi qu'à effectuer de petits aménagements avec des bois, des cordes… pour que les animaux utilisent au mieux l’espace. <br/>
+
+									Ils créent aussi des enrichissements afin de stimuler l'activité des animaux et de diminuer les comportements stéréotypés. <br/>
+
+									Comme ils les côtoient quotidiennement, ils connaissent parfaitement le caractère de chacun ainsi que toutes leurs habitudes. Ils les observent avec attention pour déceler le plus tôt possible tout comportement anormal : une maladie, une blessure, un conflit… </p>
+
+									<h2>Les vétérinaires </h2>
+									<p> Avec l’aide des soigneurs-animaliers, ile vétérinaire veille à la bonne santé des animaux. 
+									Son travail comprend, bien sûr, la mise en place de traitement pour soigner les malades, la surveillance sanitaire de toute la collection animale mais également l’établissement de traitements préventifs : campagne de vaccination… <br/>
+
+									Il intervient aussi dans la gestion de la collection en ayant en charge les transferts d’animaux. Il assure les relations avec les autorités d'administration. Il identifie les animaux du parc, pour la plupart grâce à des puces électroniques et obtient tous les documents administratifs nécessaires. <br/>
+
+									Pour devenir vétérinaire en parc zoologique, il faut suivre la filière classique. </p>
+
+									<h2> L'accueil et l'administratif </h2>
+									<p> Des agents d’entretien maintiennent la propreté du lieu en nettoyant les allées du parc quotidiennement.
+									Du personnel s'occupe de recevoir les visiteurs à l'accueil.
+									Des cuisiniers préparent les plats de la restauration rapide du snack.
+									Des vendeuses à la boutique s'occupent de l'approvisionnement de ses produits et de l'accueil des visiteurs.
+									Un graphiste s'occupe de la communication visuelle du parc.
+									Un secrétariat gère toute l'administration du parc.Et bien sûr, une équipe de direction qui supervise les activités de tout ce petit monde. </p>
+
+									<h2> L'animateur pédoagique </h2>
+									<p> L'animateur pédagogique du parc s'occupe des animations pédagogiques auprès du public, il accompagne régulièrement des petits groupes pour la découverte du métier de soigneur animalier (Soigneur d'un Jour). Il intervient également pour les ateliers lors des visites de groupes. Son rôle capital pour la sensibilisation du public est très efficace grâce à une approche particulière des animaux. </p>
+								</div>
+							</div>	
+					</main>
+
+								<!-- Footer -->
+				<?php include("footer.php");?>
+
+			</div>
+
+	</body>
+</html>
