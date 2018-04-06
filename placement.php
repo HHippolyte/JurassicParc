8e3a4521a9fdcb34a2af58e989ec1f9c1c5aa8f0
HHippolyte <36276632+HHippolyte@users.noreply.github.com>
2018-04-06 18:28:15 +0200

    Update placement.php

--- placement.php
+++ placement.php
@@ -2,8 +2,6 @@
 session_start();
 ?>
 <!DOCYPE html>
-
-
 <html>
 	<head>
 		<title>Placement animal - ZooPedia</title>
@@ -19,78 +17,69 @@ session_start();
 				<div class="colonne_alpha col12 center">
 					<div class="cadre centrer">
 						<h2>Placement des animaux existants dans les différents espaces du zoo</h2>
+						<h3>Veuillez rentrer l'ID de l'animal à placer:</h3>
 						<form method="post">
-							
-							<h3>Animal souhaité:</h3>
-							
-							<select id="animal" name="choix" size="1">
-								<?php
-
-								//Fabrication d'une requête SQL
-								$animal='SELECT distinct NUM_ANIMAL, NOM_ANIMAL FROM animal';
-								//Envoi de la reuqête SELECT
-								$result_animal=mysqli_query($connexion,$animal) or die(mysql_error());
-									while($data_animal=mysqli_fetch_array($result_animal,MYSQLI_ASSOC))
-												{
-													echo '<option value="'.$data_animal["NUM_ANIMAL"].'"-"'.$data_animal["NOM_ANIMAL"].'"</option>'.$data_animal["NUM_ANIMAL"].'-'.$data_animal["NOM_ANIMAL"].'<br/>';
-												}
-								?>
-						
-							</select>
+							<input type="number" name="animal"  value="<?php if(isset($_POST['animal'])){echo $_POST['animal'];} ?>">
+							<input type="submit">
+						</form>
 					</div>
 					<div class="cadre centrer">
-							<h3>Selection de l'espace: </h3>
-						<select  id="espace" name="choix2" size="1">
-									<?php
-												//Affichage des résultats liés aux espaces
-											$proc='SELECT distinct NUM_ESPACE, LIBELLE_ESPACE FROM espace ' ;
-											$result_espace=mysqli_query($connexion,$proc) or die(mysql_error());
-							while($res=mysqli_fetch_array($result_espace,MYSQLI_ASSOC))
-									
+						<h3> Informations de l'animal selectionné:</h3>
+						<?php
+						if(!empty($_POST))
 											{
-
-											echo '<option value="'.$res["NUM_ESPACE"].'"-"'.$res["LIBELLE_ESPACE"].'"</option>'.$res["NUM_ESPACE"].'-'.$res["LIBELLE_ESPACE"].'<br/>'; 
+												//Vérification des champs
+												if(isset($_POST['animal']) && $_POST['animal'] !="")
+													{$animal=$_POST['animal'];}
 											}
+						$requete="SELECT NUM_ANIMAL, NOM_ANIMAL, CODE_ESPECE, DATE_NAISSANCE_ANIMAL FROM animal WHERE NUM_ANIMAL=$animal";
+						$result_animal=mysqli_query($connexion,$requete) or die(mysql_error());
+												while($ligne=mysqli_fetch_array($result_animal)){
+															
+													echo 'value="ID animal: '.$ligne["NUM_ANIMAL"].'" - Nom animal: "'.$ligne["NOM_ANIMAL"].'" - Espece animal: "'.$ligne["CODE_ESPECE"].'"- Date naissance animal: "'.$ligne["DATE_NAISSANCE_ANIMAL"].'" ID animal: '.$ligne["NUM_ANIMAL"].' - Nom animal: '.$ligne["NOM_ANIMAL"].' - Code espece animal: '.$ligne["CODE_ESPECE"].'<br/> - Date naissance animal: '.$ligne["DATE_NAISSANCE_ANIMAL"].'<br/>';
+												}
+
+
+						?>
+						<form action="confirmationplacement.php" method="post">
+							<h3>Veuillez choisir un espace:</h3>
+							<select name="choixespace">
+								<?php
+								
+										
+									if(!empty($_POST))
+											{
+												//Vérification des champs
+												if(isset($_POST['animal']) && $_POST['animal'] !="")
+													{$animal=$_POST['animal'];}
+											}
+
+									$proc="CALL SP_espaces_correspondant_p_numanimal($animal)";
+									$result_espace=mysqli_query($connexion,$proc) or die(mysql_error());
+												while($ligne=mysqli_fetch_array($result_espace))
+													{
+																
+														echo '<option value="ID espace: '.$ligne["NUM_ESPACE"].'" - libelle espace: "'.$ligne["LIBELLE_ESPACE"].'" - localisation espace: "'.$ligne["LOCALISATION_ESPACE"].'"</option> ID espace: '.$ligne["NUM_ESPACE"].' - libelle espace: '.$ligne["LIBELLE_ESPACE"].' - localisation espace: '.$ligne["LOCALISATION_ESPACE"].'<br/>';
+													}
 									
-									?>
-						</select>	
-					</div>	
-					<div class="cadre centrer">	
-											<h3>Dates prévues:</h3>
-						<form>
+
+								?>
+							</select>
+							<br/>
+						<h3>Dates prévues du placement de l'animal:</h3>
+						
+									
+									<input type="hidden" name="numanimal" value="<?php if(isset($_POST['animal'])){echo $_POST['animal'];} ?>">
 									<p><label for="datedebplace" > Date de placement dans cet espace: </label></p>
 									<input type="date" name="datedebplace"> <br/> 
-									<p><label for="datefinplace" > Date de fin de placement dans cet espace : </label></p>
-									<input type="date" name="datefinarrive"> <br/> 
-									<input type="submit" value="Déplacer">
+									<p><label for="datefinplace"> Date de fin de placement dans cet espace : </label></p>
+									<input type="date" name="datefinplace"> <br/> 
+									<input type="submit" value="Enregistrer le placement">
 						</form>
-						
 					</div>
-					<div class="contenu centrer">
-						<br/>
-						<?php 
-							if(!empty($_POST))
-							{
-								$choix=$_POST['choix'];
-								$choix2=$_POST['choix2'];
-								$datedeb=$_POST['datedebplace'];
-								$datefinplace=$_POST['datefinarrive'];
-
-								$insertion="INSERT INTO placement VALUES (NULL,'$choix2','$choix','$datedeb','$datefinplace')";
-		          				$resultat =mysqli_query($connexion,$insertion);
-								echo $choix."a bien été déplacé ! <br/>"; 
-								echo "Espace : ".$choix2. "<br/>";
-								echo "Date de début de placement : ".$datedeb. "<br/>";
-								echo "Date de fin de placement : ".$datefinplace. "<br/>";
-							}
-							else 
-							{
-								echo "Veuillez renseigner l'ensemble des champs !";
-							}
-						?>
-					</div>
+				</div>
+				<!-- Footer -->
+				<?php include("footer.php");?>
 			</div>
 	</body>
-	<!-- Footer-->
-	<?php include("footer.php");?>
 </html>
